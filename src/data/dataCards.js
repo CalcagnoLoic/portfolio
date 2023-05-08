d02f272a6d4fc45b4aa50381e751e941be0b1a4b
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-05-08 09:45:10 +0200

    fix logo github

--- src/data/dataCards.js
+++ src/data/dataCards.js
@@ -3,21 +3,21 @@ export const dataCards = [
         id: 1, 
         image_project: './img/sunnyside.png',
         link_project: 'https://calcagnoloic.github.io/Sunnyside-agency-react/', 
-        logo_github: './img/github.png',
+        logo_github: './img/github.svg',
         link_github: 'https://github.com/CalcagnoLoic/Sunnyside-agency-react/tree/main',
     }, 
     {
         id: 2, 
         image_project: './img/loopstudio.png',
         link_project: 'https://calcagnoloic.github.io/loopstudios/', 
-        logo_github: './img/github.png',
+        logo_github: './img/github.svg',
         link_github: 'https://github.com/CalcagnoLoic/loopstudios',
     }, 
     {
         id: 3, 
         image_project: './img/spacetourism.png',
         link_project: 'https://space-tourism-loic.netlify.app/', 
-        logo_github: './img/github.png',
+        logo_github: './img/github.svg',
         link_github: 'https://github.com/CalcagnoLoic/space-tourism',
     }
 ]
