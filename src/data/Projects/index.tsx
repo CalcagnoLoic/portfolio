47aad7b99355f0f417c92e17fa7d29c9f1d1fd53
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-09-02 18:29:24 +0200

    adding new page for projects

---
+++ src/data/Projects/index.tsx
@@ -0,0 +1,82 @@
+export const Projects = [
+  {
+    projectId: "d$i1mf*",
+    projectIllustration: "src/assets/img/catwiki.webp",
+    projectWebsite: "https://cat-wiki-loic.netlify.app/",
+    projectTitle: "CatWiki",
+    projectStatus: false,
+    projectStack: ["next", "ts", "tw"],
+    projectLink: [
+      {
+        href: "https://github.com/CalcagnoLoic/catwiki/",
+        kind: "github",
+        css: "w-8 h-8",
+      },
+      {
+        href: "https://cat-wiki-loic.netlify.app/",
+        kind: "website",
+        css: "w-8 h-8",
+      },
+    ],
+  },
+  {
+    projectId: "?ZxC*Hi",
+    projectIllustration: "src/assets/img/planets-fact.webp",
+    projectWebsite: "https://calcagnoloic.github.io/planets-fact/",
+    projectTitle: "Planets fact",
+    projectStatus: false,
+    projectStack: ["astro", "react", "ts", "tw"],
+    projectLink: [
+      {
+        href: "https://github.com/CalcagnoLoic/planets-fact",
+        kind: "github",
+        css: "w-8 h-8",
+      },
+      {
+        href: "https://calcagnoloic.github.io/planets-fact/",
+        kind: "website",
+        css: "w-8 h-8",
+      },
+    ],
+  },
+  {
+    projectId: "?ijhfm*",
+    projectIllustration: "src/assets/img/dictionary.webp",
+    projectWebsite: "https://github.com/CalcagnoLoic/dictionary-webapp",
+    projectTitle: "Dictionary webapp",
+    projectStatus: false,
+    projectStack: ["react", "ts", "storybook", "tw"],
+    projectLink: [
+      {
+        href: "https://github.com/CalcagnoLoic/dictionary-webapp",
+        kind: "github",
+        css: "w-8 h-8",
+      },
+      {
+        href: "https://calcagnoloic.github.io/dictionary-webapp/",
+        kind: "website",
+        css: "w-8 h-8",
+      },
+    ],
+  },
+  {
+    projectId: "j*o2ky$",
+    projectIllustration: "src/assets/img/password-generator.webp",
+    projectWebsite: "https://calcagnoloic.github.io/password-generator-app/",
+    projectTitle: "Password generator",
+    projectStatus: false,
+    projectStack: ["ts", "tw"],
+    projectLink: [
+      {
+        href: "https://github.com/CalcagnoLoic/password-generator-app",
+        kind: "github",
+        css: "w-8 h-8",
+      },
+      {
+        href: "https://calcagnoloic.github.io/password-generator-app/",
+        kind: "website",
+        css: "w-8 h-8",
+      },
+    ],
+  },
+];
--- src/definitions.ts
+++ src/definitions.ts
@@ -3,7 +3,37 @@ export type navLinkProps = {
   link: string;
 };
 
-export type NetworkIconsProps = {
-  kind: "linkedin" | "gmail" | "github";
+
+interface ElementProps {
   css?: string;
+  content?: string | JSX.Element;
+  id?: string;
+}
+
+export interface SkillsProps extends ElementProps {
+  kind:
+    | "react"
+    | "ts"
+    | "js"
+    | "tw"
+    | "storybook"
+    | "sql"
+    | "python"
+    | "flask"
+    | "fastapi"
+    | "docker"
+    | "django"
+    | "git"
+    | "redux"
+    | "react-router"
+    | "astro"
+    | "next"
+    | "actions";
+}
+
+export type NetworkIconsProps = {
+  kind: "linkedin" | "github" | "gmail" | "website" | "storybook";
+  isDarkBackground?: boolean;
+  css?: string;
+  target?: string
 };
